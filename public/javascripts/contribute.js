46ea41a47c0358165721e7db293fa0962f69b0ce
Francis <meng.francis@gmail.com>
2014-02-17 14:42:44 -0500

    start search Champion

--- public/javascripts/contribute.js
+++ public/javascripts/contribute.js
@@ -5,40 +5,44 @@
  * Time: 上午3:47
  * To change this template use File | Settings | File Templates.
  */
-$(document).ready(function(){
-	$('#part').click(function(){
+$(document).ready(function () {
+	$('#part').click(function () {
 		console.log('jajaja');
 		$('#part').append('<input class = "form-control otherPart" type = "text" name = "part" placeholder="part">');
 	});
 
-	$('#addChampionSubmit').click(function(){
+	$('#championName').change(function () {
+		$('#nameForm').append('<img class="" src="http://ddragon.leagueoflegends.com/cdn/4.2.6/img/champion/' + $('#championName').val() + '.png">')
+	});
+
+	$('#addChampionSubmit').click(function () {
 		var champName = $('#championName').val();
 		var title = $('#title').val();
 		var region = $('#region').val();
 		var type = $('#type').val();
 		$.ajax({
 			url: '/addChampion',
-			type:'post',
-			data: JSON.stringify({champName:champName,title:title,region:region,type:type}),
-			dataType:'json',
-			contentType:'application/json',
-			success:function(message){
-				if(!message.err){
+			type: 'post',
+			data: JSON.stringify({champName: champName, title: title, region: region, type: type}),
+			dataType: 'json',
+			contentType: 'application/json',
+			success: function (message) {
+				if (!message.err) {
 					console.log(message.success);
 					$('.tab-content').prepend('<p style = "margin-top: 10px" class = "alert alert-success">Success: Add Champion</p>');
-					setTimeout(function(){
+					setTimeout(function () {
 						$('.alert').fadeOut();
-					},2000);
+					}, 2000);
 					$('#championName').val('');
 					$('#title').val('');
 					$('#region').val('');
 					$('#type').val('');
-				}else{
+				} else {
 					$('.tab-content').prepend('<p style = "margin-top: 10px" class = "alert alert-danger">' +
 						'Error: Champion is already exist</p>');
-					setTimeout(function(){
+					setTimeout(function () {
 						$('.alert').fadeOut();
-					},2000);
+					}, 2000);
 					$('#championName').val('');
 					$('#title').val('');
 					$('#region').val('');
@@ -46,7 +50,7 @@ $(document).ready(function(){
 				}
 
 			},
-			error:function(err){
+			error: function (err) {
 				console.log(err.err);
 			}
 		})
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var lol = lol || {};
 
 lol.route = lol.route || {};
 
-lol.fileRequire = lol.fileRequire  || {};
+lol.fileRequire = lol.fileRequire || {};
 lol.fileRequire.login = require('./login');
 lol.fileRequire.register = require('./register');
 lol.fileRequire.index = require('./hackIndex');
@@ -13,7 +13,7 @@ lol.fileRequire.itemHandler = require('./itemHandler');
  */
 exports.route = function (app) {
 	app.get('/', lol.route.get_index);
-	app.get('/logout',lol.route.get_logout);
+	app.get('/logout', lol.route.get_logout);
 	app.post('/login', lol.route.post_login);
 	app.post('/register', lol.route.post_register);
 	app.get('/contribute', lol.route.get_add_Champion);
@@ -40,27 +40,27 @@ lol.route.post_register = function (req, res) {
 	return lol.fileRequire.register.reigster(req, res);
 };
 
-lol.route.get_logout = function(req,res){
+lol.route.get_logout = function (req, res) {
 	req.session.user = null;
 	return res.redirect('/');
 };
 
-lol.route.get_profile = function(req, res){
+lol.route.get_profile = function (req, res) {
 	res.render('profile', {title: 'Profile'});
 };
 
-lol.route.get_add_Champion = function(req, res){
+lol.route.get_add_Champion = function (req, res) {
 	lol.fileRequire.championHandler.get_add_Champion(req, res);
 };
 
-lol.route.get_search_champion = function(req, res){
+lol.route.get_search_champion = function (req, res) {
 	lol.fileRequire.championHandler.get_search_champion(req, res);
 };
 
-lol.route.get_search_item = function(req, res){
+lol.route.get_search_item = function (req, res) {
 	lol.fileRequire.itemHandler.get_search_item(req, res);
 };
 
-lol.route.post_add_champion = function(req, res){
+lol.route.post_add_champion = function (req, res) {
 	lol.fileRequire.championHandler.post_add_champion(req, res);
 };
